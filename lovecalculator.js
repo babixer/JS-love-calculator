3dfe7ea2f1ff6adca789072b6be20ac87cfe54f4
Babixer <51507608+babixer@users.noreply.github.com>
2020-01-16 20:41:08 +0100

    Add files via upload

---
+++ lovecalculator.js
@@ -0,0 +1,19 @@
+// First I define my variables
+
+var personX = "Your First Name Here";
+var percrush = "Your Crushes First Name Here";
+
+// Then I made a prompt command to receive data, in this case Names 
+
+prompt(personX);
+prompt(percrush);
+
+// Then my percentage calculation using JS maths
+
+var lovePer = Math.floor(Math.random() * 101);
+
+// Finally I send alert to push my message + result 
+
+alert("Hey, Your love percentace is " + lovePer + "%");
+
+// Thanky you 😇🤪
